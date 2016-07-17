aa53ba2b42ab200c08185a7e0a8fd24d5d7d2cb8
Jonathan <poisson.jonathan777@gmail.com>
2016-07-16 23:22:48 -0400

    Learned some types

---
+++ types/types.js
@@ -0,0 +1,18 @@
+var n = "";
+n = 'Robb';
+var isDone = false;
+isDone = true;
+var count = 5;
+var names = ["Jon", "Mike"];
+var Starks;
+(function (Starks) {
+    Starks[Starks["Jon"] = 0] = "Jon";
+    Starks[Starks["Bran"] = 1] = "Bran";
+    Starks[Starks["Eddard"] = 2] = "Eddard";
+    Starks[Starks["Catlyn"] = 3] = "Catlyn";
+})(Starks || (Starks = {}));
+;
+var cat = Starks.Catlyn;
+function getName() {
+    return "Brad";
+}
---
+++ types/types.ts
@@ -0,0 +1,17 @@
+var n : string = "";
+
+n = 'Robb';
+
+var isDone: boolean = false;
+isDone = true;
+
+var count : number = 5;
+
+var names : string[] = ["Jon", "Mike"];
+
+enum Starks {Jon, Bran, Eddard, Catlyn};
+var cat : Starks = Starks.Catlyn;
+
+function getName() : string {
+  return "Brad";
+}
